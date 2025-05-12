f69f3a6a45dc536becf3c3d81189f41da86950c1
MaksymilianBeta <maxxb2004@gmail.com>
2025-05-12 16:21:41 +0200

    Add files via upload

---
+++ Ćw. 4/trivia bot.js
@@ -0,0 +1,13 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName="Maksiu";
+let botLocation="Skierniewice";
+let favoriteLanguage="SQL";
+console.log("My name is "+ botName +" and I live on " +botLocation+ ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact = favoriteLanguage + " is the best programming language that i ever learned";
+console.log(codingFact);
+codingFact="I hope i will work with "+ favoriteLanguage +".";
+console.log(codingFact);
+codingFact="I'm looking forward to learn "+ favoriteLanguage +"next year on university" +".";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation+ ".")
